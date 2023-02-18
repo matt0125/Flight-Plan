4b5216c81d5d163ea5c345cb92e3dad0e8c02efb
Matthew Daley <m.daley0125@gmail.com>
2023-02-18 16:25:31 -0500

    First session
    Got a lot done; read matrix, created time controller thread, thread creater/run, locks and all that jazz

--- Project.java
+++ Project.java
@@ -1,6 +1,9 @@
 import java.util.*;
 import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
+import java.util.concurrent.locks.ReentrantReadWriteLock.*;
 import java.io.*;
+import java.lang.Thread;
 
 
 // Input: Weighted undirected graph
@@ -13,30 +16,49 @@ public class Project
 {
   private static int[][] _matrix;
   private static int _numNodes;
-  private static int _turnTime;
+  private static int [] _turnTimeMaster;
+  private static int [] _turnTime;
+  private static ReentrantReadWriteLock _lock = new ReentrantReadWriteLock();
+
   private static final int INFINITY = (int) 10E8;
 
-  public static void main(String [] args) throws FileNotFoundException
+  public static void main(String [] args) throws FileNotFoundException, InterruptedException
   {
-    // Fix later
+    AtomicInteger counter = new AtomicInteger(0);
+
+    // Change input to be agrs[0]
     readMatrix("input.txt");
-    //multiThread[] thr = new multiThread[];
     printMatrix();
 
-    int [][] j = new int [7][7];
-    System.out.println(j[0][0]);
-
+    TimeController timeController = new TimeController(_turnTime, _turnTimeMaster, _lock);
+    Thread t1 = new Thread(timeController);
+    t1.start();
+    
+    createThreads(counter);
   }
 
-  public static void test(int [][] g)
+  public static void createThreads(AtomicInteger counter) throws InterruptedException
   {
-    g[0][0] = 1;
+    Thread[] threads = new Thread[7];
+
+    for (int i = 0; i < 7; i++)
+    {
+      threads[i] = new Thread(new multiThread(_matrix, _matrix, _numNodes, _lock, counter));
+      threads[i].start();
+    }
+
+    for (int i = 0; i < 7; i++)
+    {
+      threads[i].join();
+    }
   }
 
   // returns 1d int array of length _numNodes
   public static int[] dijkstras(int source)
   {
+    // Time to get to each node from source
     int [] time = new int[_numNodes];
+    
     boolean [] visited = new boolean[_numNodes];
 
     for (int i = 0; i < _numNodes; i++)
@@ -45,7 +67,7 @@ public class Project
       visited[i] = false;
     }
 
-    time[source] = 0; // Source to source -> no travel
+    time[source] = _turnTime[source]; // Source to source -> just wait time
 
     for (int i = 0; i < _numNodes; i++)
     {
@@ -54,9 +76,9 @@ public class Project
 
       for (int j = 0; j < _numNodes; j++)
       { 
-        if (visited[j] == false && time[j] <= min)
+        if (visited[j] == false && (time[j] + _turnTime[j]) <= min)
         {
-          min = time[j];
+          min = (time[j] + _turnTime[j]);
           nextNode = j;
         }
       }
@@ -66,8 +88,8 @@ public class Project
       for (int j = 0; j < _numNodes; j++)
       {
         if (!visited[j] && _matrix[i][j] != 0 && time[i] != INFINITY
-            && time[i] + _matrix[i][j] < time[j])
-            time[j] = time[i] + _matrix[i][j];
+            && (time[i] + _turnTime[i] + _matrix[i][j]) < time[j])
+            time[j] = (time[i] + _turnTime[i] + _matrix[i][j]);
       }
     }
 
@@ -79,10 +101,10 @@ public class Project
     Scanner scan = new Scanner(new File(filename));
 
     _numNodes = scan.nextInt();
-
-    _turnTime = scan.nextInt(); // time taken to swap between loading and running
-
+    
     _matrix = new int[_numNodes][_numNodes]; // matrix that holds the distance between nodes
+    _turnTime = new int[_numNodes];
+    _turnTimeMaster = new int[_numNodes];
     
     
     for (int i = 0; i < _numNodes; i++)
@@ -94,6 +116,12 @@ public class Project
         if(_matrix[i][j] == -1)
           _matrix[i][j] = INFINITY;
       }
+    }
+
+    for (int i = 0; i < _numNodes; i++)
+    {
+      _turnTimeMaster[i] = scan.nextInt();
+      _turnTime[i] = _turnTimeMaster[i];
     }
   }
 
@@ -108,9 +136,78 @@ public class Project
       System.out.println();
     }
   }
+
+  public static int[] getTime(ReadLock lock)
+  {
+    try
+    {
+      // Wait for unlock
+      lock.lock();
+
+      return _turnTime;
+    }
+    finally{
+      // Unlock
+      lock.unlock();
+    }
+
+    // _lockTime = TimeController.getTime()
+  }
 }
 
+class TimeController implements Runnable
+{
+  int[] _turnTime;
+  int[] _turnTimeMaster;
+  WriteLock _writeLock;
 
+  public TimeController(int [] turnTime, int [] turnTimeMaster, ReentrantReadWriteLock lock)
+  {
+    _turnTime = turnTime;
+    _turnTimeMaster = turnTimeMaster;
+    _writeLock = lock.writeLock();
+  }
+
+  @Override
+  public void run()   
+  {
+    while(true)
+    {
+      try
+      {
+        // Lock
+        _writeLock.lock();
+        
+        // Update array
+        for (int i = 0; i < _turnTime.length; i++)
+        {
+          _turnTime[i]--;
+
+          if (_turnTime[i] == -1)
+          {
+            _turnTime[i] = _turnTimeMaster[i];
+          }
+        }
+      }
+      finally
+      {
+        // Unlock array
+        _writeLock.unlock();
+      }
+       // Wait
+       synchronized(this)
+       {
+          try 
+          {
+            this.wait(1000);
+          } catch (InterruptedException e) 
+          {
+            e.printStackTrace();
+          }
+       }
+    }
+  }
+}
 
 
 class multiThread extends Thread
@@ -118,23 +215,34 @@ class multiThread extends Thread
   int source; // the source vertex
   int result[];
   int[][] _matrix;
+  int[][] _times;
   int _numNodes;
   AtomicInteger _counter;
+  ReadLock _lock;
   private final int INFINITY = (int) 10E8;
 
-  public multiThread(int matrix[][], int numNodes, AtomicInteger counter)
+  public multiThread(int matrix[][], int times[][], int numNodes, ReentrantReadWriteLock lock, AtomicInteger counter)
   {
-    this.source = counter.get();
+    this.source = counter.get(); // source to work on is whatever the counter was
+    counter.getAndIncrement();  // counter increases so threads can work on other sources
     this._matrix = matrix;
+    this._times = times;
     this._numNodes = numNodes;
+    _lock = lock.readLock();
     _counter = counter;
   }
 
 
+  
   // returns 1d int array of length _numNodes
   public int[] dijkstras(int source)
   {
+
+    int [] turnTime = new int[_numNodes];
+
+    // Time to get to each node from source
     int [] time = new int[_numNodes];
+    
     boolean [] visited = new boolean[_numNodes];
 
     for (int i = 0; i < _numNodes; i++)
@@ -143,7 +251,7 @@ class multiThread extends Thread
       visited[i] = false;
     }
 
-    time[source] = 0; // Source to source -> no travel
+    time[source] = turnTime[source]; // Source to source -> just wait time
 
     for (int i = 0; i < _numNodes; i++)
     {
@@ -152,9 +260,9 @@ class multiThread extends Thread
 
       for (int j = 0; j < _numNodes; j++)
       { 
-        if (visited[j] == false && time[j] <= min)
+        if (visited[j] == false && (time[j] + turnTime[j]) <= min)
         {
-          min = time[j];
+          min = (time[j] + turnTime[j]);
           nextNode = j;
         }
       }
@@ -164,23 +272,21 @@ class multiThread extends Thread
       for (int j = 0; j < _numNodes; j++)
       {
         if (!visited[j] && _matrix[i][j] != 0 && time[i] != INFINITY
-            && time[i] + _matrix[i][j] < time[j])
-            time[j] = time[i] + _matrix[i][j];
+            && (time[i] + turnTime[i] + _matrix[i][j]) < time[j])
+            time[j] = (time[i] + turnTime[i] + _matrix[i][j]);
       }
     }
+
     return time;
   }
+  
 
 
   @Override
   public void run()   // what the thread will do
   {
     // will run dij algorithm and find the shortest path for every vertex from a given source
-
-    // we need to call dij from this class so we need to move the method from project to here
-
-    // perhaps we could make a 2d matrix in project1 to store result. This way, threads can move on w/o having to wait for another thread
-    while()
-      result = dijkstras(source);
+    
+    result = dijkstras(source); // still need to store result
   }
 }
